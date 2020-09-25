41d0b11c7ce950a150c2e92bb3586367cb3d8cf1
Braham Prakash <developer.brm@gmail.com>
2020-09-25 21:39:18 +0530

    Updates

---
+++ precache-manifest.107481d5275f3aaf5ebb63300898f204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0672efc894c4ff9ef65b962199bcc2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "82d581d498c9fc95ec84",
+    "url": "/calculator/static/css/main.606cfe7e.chunk.css"
+  },
+  {
+    "revision": "57470963cd8bf4ff2539",
+    "url": "/calculator/static/js/2.035621a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.035621a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d581d498c9fc95ec84",
+    "url": "/calculator/static/js/main.7425fd14.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
